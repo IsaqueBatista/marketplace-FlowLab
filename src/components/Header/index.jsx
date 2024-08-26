5002ebc111f0a2ba5f15f9f62ddcf059b09f5aef
isaquebatista <isaquebatista1716@gmail.com>
2024-08-26 11:05:17 -0300

    Header created

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import styles from "./Header.module.css";
+import { Inter } from "next/font/google";
+import Link from "next/link";
+
+import Logo from "@/public/images/logo.png";
+import Cart from "@/public/images/cart.svg";
+import User from "@/public/images/user.svg";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <Link href="/">
+            <Image src={Logo} 
+            alt="Logo Flow Lab" 
+            width={147} 
+            height={24} />
+          </Link>
+        </div>
+
+        <nav className={styles.navigation}>
+          <div className={styles.navItem}>
+          <Link href="#" className={styles.navLink}>
+            <Image src={Cart} 
+            alt="Ícone do carrinho" 
+            width={13} 
+            height={11} />
+            <p className={`${styles.navText} ${inter.className}`}>Carrinho</p>
+            </Link>
+          </div>
+
+          <div className={styles.navItem}>
+          <Link href="#" className={styles.navLink}>
+            <Image src={User} 
+            alt="Ícone de usuário" 
+            width={13} 
+            height={11} />
+            <p className={`${styles.navText} ${inter.className}`}>Entrar</p>
+            </Link>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Layout from "../components";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Marketplace Flow Lab</title>
+        <meta name="description" content="Marketplace Flow Lab" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout />
+    </>
+  );
+}
