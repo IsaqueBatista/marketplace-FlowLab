8cb730e1ece7c44a2a1f5cb7ee53dc7b87d2ced5
isaquebatista <isaquebatista1716@gmail.com>
2024-08-28 09:39:52 -0300

    Add ProductGrid to Component Main

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,19 +1,31 @@
+import { useState } from "react";
 import Image from "next/image";
 import ShoesMain from "@/public/images/shoesMain.svg";
 import styles from "./Main.module.css";
 
-export default function Main() {
+import ProductGrid from "../ProductGrid";
+
+export default function Main({ initialProducts }) {
+  const [products] = useState(initialProducts || []);
+
   return (
-    <div className={styles.container}>
-      <div className={styles.divTittle}>
-        <h1 className={styles.tittle}>Estilo e conforto para os seus pés</h1>
+    <>
+      <div className={styles.container}>
+        <div className={styles.divTittle}>
+          <h1 className={styles.tittle}>Estilo e conforto para os seus pés</h1>
+        </div>
+        <div className={styles.divShoes}>
+          <Image
+            src={ShoesMain}
+            className={styles.image}
+            alt="Shoes Main Image"
+          />
+        </div>
       </div>
-      <div className={styles.divShoes}>
-        <Image
-          src={ShoesMain}
-          className={styles.image}
-        ></Image>
-      </div>
-    </div>
+
+      <main className={styles.main}>
+        <ProductGrid products={products} />
+      </main>
+    </>
   );
 }
